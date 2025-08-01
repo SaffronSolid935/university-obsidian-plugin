5078c7001d6309905817ae0047bedc7b7e5e653e
SaffronSolid935 <98280209+SaffronSolid935@users.noreply.github.com>
2025-08-01 18:30:34 +0200

    Init + some pre design

--- main.ts
+++ main.ts
@@ -1,134 +1,70 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {Notice, Plugin, WorkspaceLeaf} from 'obsidian';
+import { UniversityView, VIEW_UNIVERSITY } from 'view';
+import { DEFAULT_SETTINGS, UniversityPluginSettings, UniversitySettingsTab } from 'settings';
+// import navbarCSS from "./styles/navbar.css";
 
-// Remember to rename these classes and interfaces!
+// const STYLESHEETS: Array<string> = [
+//     navbarCSS
+// ];
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class UnivresityPlugin extends Plugin
+{
+    settings: UniversityPluginSettings;
+    async onload()
+    {
+        await this.loadSettings();
+        // this.loadStyles();
+        this.registerView(
+            VIEW_UNIVERSITY,
+            (leaf) => new UniversityView(leaf,this)
+        );
+        this.addRibbonIcon('graduation-cap','University',(evt) => this._openSidebar());
+        // ribbonIcon.addClass('my-plugin-ribbon-class');
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+        this.addSettingTab(new UniversitySettingsTab(this.app, this));
+    }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    // loadStyles()
+    // {
+    //     const pluginFolder = this.manifest.dir;
 
-	async onload() {
-		await this.loadSettings();
+    //     STYLESHEETS.forEach(file => {
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    //         const linkEl = document.createElement('style');
+    //         linkEl.textContent = file;
+    //         document.head.appendChild(linkEl);
+    //     });
+    // }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    async _openSidebar() {
+        // new UniversityView();
+        const { workspace } = this.app;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        let leaf: WorkspaceLeaf | null = null;
+        const leaves = workspace.getLeavesOfType(VIEW_UNIVERSITY);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        if (leaves.length > 0)
+        {
+            leaf = leaves[0];
+        } else 
+        {
+            leaf = workspace.getLeftLeaf(false);
+            await leaf?.setViewState({ type: VIEW_UNIVERSITY, active: true});
+        }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        if (leaf != null)
+            workspace.revealLeaf(leaf);
+        else
+            new Notice('Error');
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    async loadSettings() 
+    {
+        this.settings = Object.assign({},DEFAULT_SETTINGS, await this.loadData());
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    async saveSettings()
+    {
+        await this.saveData(this.settings);
+    }
 }
