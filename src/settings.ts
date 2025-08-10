017f10e6e608294f804ff3ded079b5442d4e2984
SaffronSolid935 <98280209+SaffronSolid935@users.noreply.github.com>
2025-08-10 12:42:52 +0200

    doc: Documented settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,11 @@
 import UnivresityPlugin from "main";
 import { App, PluginSettingTab, Setting } from "obsidian";
 
+//#region Settings-Defenition
+
+/**
+ * Template for the university plugin settings. It is used in the Settings-Tab of the university plugin.
+ */
 export interface UniversityPluginSettings
 {
     timeformat: string,
@@ -10,6 +15,9 @@ export interface UniversityPluginSettings
     modules: Array<Array<string>>;
 }
 
+/**
+ * This are the default settings and this is used, when there is no setting for a specific property.
+ */
 export const DEFAULT_SETTINGS: UniversityPluginSettings = {
     timeformat: 'en-EN',
     semesters: 6,
@@ -22,16 +30,46 @@ export const DEFAULT_SETTINGS: UniversityPluginSettings = {
     ]
 }
 
+//#endregion
 
+/**
+ * This is the official university settings tab. 
+ * Normally instantiated automatically by the plugin.
+ */
 export class UniversitySettingsTab extends PluginSettingTab {
-    plugin: UnivresityPlugin;
+    
+    //#region Properties
+    /**
+     * This is the reference to the modified university plugin.
+     */
+    private plugin: UnivresityPlugin;
 
-    constructor(app: App, plugin: UnivresityPlugin)
+    /** 
+     * SaveSettings is method, which is setted dynamically. This method is used to save the settings permamently.
+     * (No worries, it will be automatically setted by the university plugin)
+     */
+    private saveSettings: () => Promise<any>;
+    //#endregion
+
+
+    /**
+     * Creates an instance of the university settings tab. Will be automatically created by the university plugin.
+     * @param app 
+     * @param plugin 
+     * @param saveSettingsMethod This method is used to save the settings, mich will be setted mostly here.
+     */
+    constructor(app: App, plugin: UnivresityPlugin, saveSettingsMethod: ()=>Promise<any>)
     {
         super(app,plugin);
         this.plugin = plugin;
+        this.saveSettings = saveSettingsMethod;
     }
 
+//#region Lifecycle
+
+    /**
+     * Generates the whole settings tab.
+     */
     display(): void {
         const {containerEl} = this;
 
@@ -45,7 +83,7 @@ export class UniversitySettingsTab extends PluginSettingTab {
                 .setValue(this.plugin.settings.timeformat)
                 .onChange( async (value)=>{
                     this.plugin.settings.timeformat = value;
-                    await this.plugin.saveSettings();
+                    await this.saveSettings();
                 })
             );
 
@@ -57,7 +95,7 @@ export class UniversitySettingsTab extends PluginSettingTab {
                 .setValue(this.plugin.settings.semesters.toString())
                 .onChange( async (value) => {
                     this.plugin.settings.semesters = await this.settingToNumber(value);
-                    await this.plugin.saveSettings();
+                    await this.saveSettings();
                     this.display();
                 })
             );
@@ -70,7 +108,7 @@ export class UniversitySettingsTab extends PluginSettingTab {
             if (this.plugin.settings.modules[i] == null)
             {
                 this.plugin.settings.modules[i] = [];
-                this.plugin.saveSettings();
+                this.saveSettings();
             }
         }
     
@@ -85,7 +123,7 @@ export class UniversitySettingsTab extends PluginSettingTab {
                         let newLength = await this.settingToNumber(value);
                         let newArray = this.resizeArray<string>(modulesInSemester,newLength);
                         this.plugin.settings.modules[i] = newArray;
-                        this.plugin.saveSettings();
+                        this.saveSettings();
                         this.display();
                     })
                 );
@@ -98,14 +136,25 @@ export class UniversitySettingsTab extends PluginSettingTab {
                         .setValue(module)
                         .onChange( async (value) => {
                             this.plugin.settings.modules[i][j] = value;
-                            this.plugin.saveSettings();
+                            this.saveSettings();
                         })
                     );
             });
         });
     }
 
-    async settingToNumber(value: string, defaultValue: number = -1) : Promise<number> {
+    //#endregion
+
+    //#region Tools
+
+    /**
+     * Converts the string to a number. This is used due the input elements for the settings do not have the input type number.
+     * @param value It expects the string value from the setting.
+     * @param defaultValue 
+     * @returns 
+     */
+
+    private async settingToNumber(value: string, defaultValue: number = -1) : Promise<number> {
         const numberValue = Number(value);
         if (!isNaN(numberValue))
         {
@@ -114,7 +163,13 @@ export class UniversitySettingsTab extends PluginSettingTab {
         return defaultValue;
     }
 
-    resizeArray<T>(old: Array<T>, newSize: number) : Array<T>
+    /**
+     * Resizes an array in a more traditionally way.
+     * @param old 
+     * @param newSize 
+     * @returns 
+     */
+    private resizeArray<T>(old: Array<T>, newSize: number) : Array<T>
     {
         let newArray: Array<T> = [];
         for (var i = 0; i < newSize; i++)
@@ -123,4 +178,6 @@ export class UniversitySettingsTab extends PluginSettingTab {
         }
         return newArray;
     }
+
+    //#endregion
 }
